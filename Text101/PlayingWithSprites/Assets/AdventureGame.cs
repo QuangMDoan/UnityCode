1e1c989cb77799bf150cf36c1507aaf228604ed2
quang <quang.m.doan@gmail.com>
2019-05-12 19:07:34 -0700

    Code text101 tirvial game.

---
+++ Text101/PlayingWithSprites/Assets/AdventureGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    //New instance variable
+    [SerializeField]Text textComponent;
+
+	// Use this for initialization
+	void Start () {
+        textComponent.text = ("Once upon a time...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
